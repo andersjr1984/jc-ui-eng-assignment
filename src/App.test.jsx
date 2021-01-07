f92a9f72d8c0b34c2647fc7671af57971fad9113
andersjr1984 <anderson.jasonryan@gmail.com>
2021-01-06 22:04:53 -0700

    a little more inital setup

---
+++ src/App.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import App from './App';
+
+const renderContainer = () => render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
+
+test('renders learn react link', () => {
+  const { getByText } = renderContainer();
+
+  expect(getByText(/learn/i)).toBeInTheDocument();
+});
