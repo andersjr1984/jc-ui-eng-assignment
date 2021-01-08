c92d7a1b0db713a8a8016e373d961d08c8b4de8f
andersjr1984 <anderson.jasonryan@gmail.com>
2021-01-08 10:57:35 -0700

    fixing tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core';
 import store from './app/store';
 import App from './App';
+import theme from './app/theme';
+
+// would typically mock axios so tests won't have errors when ran without server.
 
 const renderContainer = () =>
   render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   );
 
 test('renders learn react link', () => {
   const { getByText } = renderContainer();
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Jump Cloud/i)).toBeInTheDocument();
 });
